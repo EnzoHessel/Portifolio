430405a898fd92ee8ec45df658231ce450eedba9
Enzo Hessel <enzo.carmo@salg.com.br>
2024-04-01 15:54:55 -0300

    Portifolio
    need add images and projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Home from './components/Home';
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Portfolio />
 
       <SocialLinks />
     </div>
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+// imagens do protifolio
+// import EnzpImage from '../assets/enzo.jpg'
+
+const Portfolio = () => {
+    const portfolios = [
+        {
+            id: 1,
+            src: 'ne.jpg'
+        },
+        {
+            id: 2,
+            src: 'ne.jpg'
+        },
+        {
+            id: 3,
+            src: 'ne.jpg'
+        },
+        {
+            id: 4,
+            src: 'ne.jpg'
+        }
+    ]
+
+  return (
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
+            <p className='py-6'>Check out some of my work right here</p>
+        </div>
+
+        <div className='grid sm:grid-col-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+        {portfolios.map(({ id, src }) => (
+            <div key={id}  className='shadow-md shadow-gray-600 rounded-lg'>
+                <img src={src} alt="teste" className='rounded-md duration-200 hover:scale-105'/>
+                <div className='flex items-center justify-center'>
+                    <button className='w-1/2 px-6 py-3 m-5 duration-200 hover:scale-105'>Demo</button>
+                    <button className='w-1/2 px-6 py-3 m-5 duration-200 hover:scale-105'>Code</button>
+                </div>
+            </div>
+        ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
