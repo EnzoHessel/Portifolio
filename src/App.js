2864c57ef6406151772bd650ab10b50a64054d79
Enzo Hessel <enzo.carmo@salg.com.br>
2024-04-01 14:30:49 -0300

    add about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar";
 import Home from './components/Home';
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
 
       <SocialLinks />
     </div>
---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-20'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at nibh eu lorem finibus posuere. Nunc vitae mauris augue. Donec eleifend justo vitae placerat euismod. Donec ipsum nunc, luctus et cursus sit amet, volutpat ac justo. Fusce quis enim quis velit ullamcorper mollis. Cras vehicula vehicula nunc, non porttitor elit gravida in. Phasellus tempus fermentum leo et posuere. Aenean ac enim consectetur tortor blandit rhoncus. Phasellus sodales dictum est sit amet mattis. Ut sodales, dolor quis feugiat cursus, metus leo congue diam, sit amet mollis mauris lectus aliquam nisi. Nunc bibendum felis et risus vehicula viverra. Vivamus commodo lacus mauris, nec rutrum dolor cursus vitae. Mauris cursus id lacus eu auctor. Vivamus pulvinar id arcu quis molestie.</p>
+        <br />
+        <p className='text-xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at nibh eu lorem finibus posuere. Nunc vitae mauris augue. Donec eleifend justo vitae placerat euismod. Donec ipsum nunc, luctus et cursus sit amet, volutpat ac justo. Fusce quis enim quis velit ullamcorper mollis. Cras vehicula vehicula nunc, non porttitor elit gravida in. Phasellus tempus fermentum leo et posuere. Aenean ac enim consectetur tortor blandit rhoncus. Phasellus sodales dictum est sit amet mattis. Ut sodales, dolor quis feugiat cursus, metus leo congue diam, sit amet mollis mauris lectus aliquam nisi. Nunc bibendum felis et risus vehicula viverra. Vivamus commodo lacus mauris, nec rutrum dolor cursus vitae. Mauris cursus id lacus eu auctor. Vivamus pulvinar id arcu quis molestie.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
