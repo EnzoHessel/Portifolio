53f71025b72bcb64621d97d508a489e619f4642e
Enzo Hessel <enzo.carmo@salg.com.br>
2024-04-02 00:27:51 -0300

    svg + adjustment navbar portfoio experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,6 +6,10 @@ import reactImage from '../assets/portifolio/react.svg'
 import github from '../assets/portifolio/github.svg'
 import tailwind from '../assets/portifolio/tailwind.svg'
 import vue from '../assets/portifolio/vue.svg'
+import bootstrap from '../assets/portifolio/bootstrap.svg'
+import typeScript from '../assets/portifolio/typescript.svg'
+import vite from '../assets/portifolio/vite.svg'
+import next from '../assets/portifolio/next.svg'
 
 const Experience = () => {
     const images = [
@@ -47,15 +51,39 @@ const Experience = () => {
         },
         {
             id: 7,
+            label: 'Bootstrap',
+            src: bootstrap,
+            style: 'shadow-purple-800'
+        },
+        {
+            id: 8,
             label: 'Vue',
             src: vue,
             style: 'shadow-green-500'
+        },
+        {
+            id: 9,
+            label: 'Typescript',
+            src: typeScript,
+            style: 'shadow-blue-500'
+        },
+        {
+            id: 10,
+            label: 'Next.js',
+            src: next,
+            style: 'shadow-white'
+        },
+        {
+            id: 11,
+            label: 'Vite',
+            src: vite,
+            style: 'shadow-purple-400'
         },
     ]
     
   return (
     <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
-      <div className='max-w-screen-lg mx-auto p-4 flex flex-col w-full h-full text-white'>
+      <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
         <div>
             <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
             <p className='py-6'>Essas são as tecnologias que ja trabalhei</p>
@@ -63,8 +91,8 @@ const Experience = () => {
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
         {images.map(({id, label, src, style}) => (
-            <div key={id} className={'shadow-md hover:scale-105 duration-500 py-2 rounded-lg' + " " + style}>
-                <img src={src} alt={label} className='w-20 mx-auto'/>
+            <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                <img src={src} alt={label} className='w-20 h-20 mx-auto'/>
                 <p className='mt-4'>{label}</p>
             </div>
         ))}
