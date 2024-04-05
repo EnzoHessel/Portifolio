4922386857dde7692c41fd482c4e1f8b09036308
Enzo Hessel <enzo.carmo@salg.com.br>
2024-04-05 12:19:58 -0300

    ajustes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
+        <div className='pb-32'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
         <p className='text-xl mt-20'>Olá, sou Enzo! <br /><br />
