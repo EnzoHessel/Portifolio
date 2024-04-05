e50fc188fe5c0ae38a6e68de77ca5d5989dab211
Enzo Hessel <enzo.carmo@salg.com.br>
2024-04-05 12:55:44 -0300

    ajuste de alinhamento

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -82,17 +82,17 @@ const Experience = () => {
     ]
 
   return (
-    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+    <div name="experience" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
       <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
         <div>
             <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
             <p className='py-6'>Essas são as tecnologias que ja trabalhei</p>
         </div>
 
-        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0 pb-36'>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
         {images.map(({id, label, src, style}) => (
             <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
-                <img src={src} alt={label} className='w-20 h-20 mx-auto'/>
+                <img src={src} alt={label} className='w-20 mx-auto'/>
                 <p className='mt-4'>{label}</p>
             </div>
         ))}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div name="home" className='flex h-screen bg-gradient-to-b from-black to-gray-800 via-black'>
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
-            <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm Front-end Developer</h2>
+            <h2 className='text-4xl  font-bold text-white'>I'm Front-end Developer</h2>
             <p className='text-gray-500 py-4 max-w-md'>
               Sou desenvolvedor Front-end com experiência em desenvolvimento de sites e dispositivos móveis. Confira meus projetos e habilidades
             </p>
